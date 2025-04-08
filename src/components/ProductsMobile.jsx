9d178aa7430a478bc860d74ec65a2910c2ee18d1
MIKOXO <mikermount19@yahoo.com>
2025-04-08 16:26:41 +0300

    Updated and Finalgit add .

--- src/components/ProductsMobile.jsx
+++ src/components/ProductsMobile.jsx
@@ -23,7 +23,7 @@ const ProductsMobile = () => {
   return (
     <section className="">
       {/* Grid Section */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mt-16">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-7 mt-16">
         {cards.map((card, index) => (
           <div
             key={index}
